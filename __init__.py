fa2c1624f89e13d58794af68acba0774c4b7a19f
Laurentiu Piciu <laurentiupiciu@gmail.com>
2022-10-03 19:52:54 +0300

    paths

--- __init__.py
+++ __init__.py
@@ -1,5 +1,4 @@
 from .setup_logging import configure_logging
 from .logger_wrapper import LoggerWrapper
 
-configure_logging('log/config_logger.json')
-lw = LoggerWrapper(lib_name='data_analysis')
+configure_logging('pylogger/config_logger.json')
