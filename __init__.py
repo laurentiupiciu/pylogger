c4687190ba565dbfcb63874a95ccd39078da0257
Laurentiu Piciu <laurentiupiciu@gmail.com>
2022-10-03 20:06:50 +0300

    automatically creating from default config template

--- __init__.py
+++ __init__.py
@@ -1,4 +1,15 @@
+import os
+import shutil
+
 from .setup_logging import configure_logging
 from .logger_wrapper import LoggerWrapper
 
-configure_logging('pylogger/config_logger.json')
+path_config_logger_template = 'pylogger/.config_logger.json'
+path_config_logger = 'pylogger/config_logger.json'
+
+if not os.path.exists(path_config_logger):
+  shutil.copy(path_config_logger_template, path_config_logger)
+
+configure_logging(path_config_logger)
+
+
